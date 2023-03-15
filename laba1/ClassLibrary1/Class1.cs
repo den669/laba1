05ce56b67b624eb40212d2acd6670249271c7691
den669 <71308629+den669@users.noreply.github.com>
2023-03-15 09:59:58 +0200

    Add files via upload

---
+++ laba1/ClassLibrary1/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace Mylab1
+{
+    // Клас абстрактної кнопки
+    public abstract class Button
+    {
+        public abstract void Render();
+    }
+
+    // Клас абстрактної фабрики кнопок
+    public abstract class ButtonFactory
+    {
+        public abstract Button CreateButton();
+    }
+
+    // Класи конкретних фабрик кнопок
+    public class WindowsButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new WindowsButton();
+        }
+    }
+
+    public class MacButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new MacButton();
+        }
+    }
+
+    public class LinuxButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new LinuxButton();
+        }
+    }
+
+    // Класи конкретних кнопок
+    public class WindowsButton : Button
+    {
+        public override void Render()
+        {
+            Console.WriteLine("Rendering Windows button");
+        }
+    }
+
+    public class MacButton : Button
+    {
+        public override void Render()
+        {
+            Console.WriteLine("Rendering Mac button");
+        }
+    }
+
+    public class LinuxButton : Button
+    {
+        public override void Render()
+        {
+            Console.WriteLine("Rendering Linux button");
+        }
+    }
+
+}
---
+++ laba1/laba1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Mylab1;
+
+ButtonFactory windowsFactory = new WindowsButtonFactory();
+Button windowsButton = windowsFactory.CreateButton();
+windowsButton.Render();
+
+
+ButtonFactory macFactory = new MacButtonFactory();
+Button macButton = macFactory.CreateButton();
+macButton.Render();
+
+
+ButtonFactory linuxFactory = new LinuxButtonFactory();
+Button linuxButton = linuxFactory.CreateButton();
+linuxButton.Render();
